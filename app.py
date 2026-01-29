18319882dc6db0a8b36b77be0c39ba7a5209feee
Vemulavenkatasai <128966937+Vemulavenkatasai@users.noreply.github.com>
2026-01-29 07:18:26 +0530

    Add import for keras from tensorflow

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import load_model
